37fe0f714627f346ef11a1919d532365d230d96a
Nguyen Mai Linh <nguyenmailinhnana@gmail.com>
2024-11-13 02:13:24 +0700

    repush

---
+++ GongChaWebSale/GongChaWebSale/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GongChaWebSale.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index(string Email, string PassWord)
+        {
+
+            return View();
+        }
+        public ActionResult Register(string LastName, string FirstName, string PhoneNumber, string Email, string PassWord)
+        {
+
+            return View();
+        }
+    }
+}
