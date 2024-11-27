06d11976e3e56ef7640404538545189ab27db5ab
Nguyen Mai Linh <nguyenmailinhnana@gmail.com>
2024-11-28 06:35:55 +0700

    hoan thanh trang chu, update trang khuyenmai

--- GongChaWebSale/GongChaWebSale/Controllers/SaleOffController.cs
+++ GongChaWebSale/GongChaWebSale/Controllers/SaleOffController.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data;
 using GongChaWebSale.Models;
+
 namespace GongChaWebSale.Controllers
 {
-    public class HomeController : Controller
+    public class SaleOffController : Controller
     {
-        // GET: Home
+        // GET: SaleOff
         mydbcontext db = new mydbcontext();
-        public ActionResult Index()
+        public ActionResult Index(string makm)
         {
-            List<KhuyenMai> khuyenmai = db.khuyenMais.Where(row=>row.Size == "M" && row.NgayKetThuc>DateTime.Now).ToList();
-            
+            List<KhuyenMai> khuyenmai = db.khuyenMais.Where(row => row.Size == "M" && row.NgayKetThuc > DateTime.Now).ToList();
+            ViewBag.makm = makm;
             return View(khuyenmai);
         }
     }
