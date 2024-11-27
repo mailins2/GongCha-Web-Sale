27652a25e2b2c915f12eb96aaf54cd98b1694cd1
Nguyen Mai Linh <nguyenmailinhnana@gmail.com>
2024-11-28 00:24:35 +0700

    them data

---
+++ dbo.loaisps.data.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[loaisps] ON
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (1, N'Kem')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (2, N'Trà Trái Cây')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (3, N'Okinawa')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (4, N'Latta Series')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (5, N'Thức uống đặc biệt GongCha')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (6, N'Trà sữa')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (7, N'Trà nguyên chất')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (8, N'Thức uống sáng tạo')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (9, N'Thức uống đá xay')
+INSERT INTO [dbo].[loaisps] ([MaLoaiSP], [TenLoaiSP]) VALUES (10, N'Topping')
+SET IDENTITY_INSERT [dbo].[loaisps] OFF
---
+++ dbo.toppings.data.sql
@@ -0,0 +1,11 @@
+SET IDENTITY_INSERT [dbo].[toppings] ON
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (1, N'Kem Sữa ', CAST(17000.00 AS Decimal(18, 2)), N'~/Images/Topping/Kem-Sữa.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (2, N'Nha Đam', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Nha-Đam.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (3, N'Trân Châu Đen', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Trân-Châu-Đen.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (4, N'Trân Châu Trắng', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Trân-Châu-Trắng.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (5, N'Đậu Đỏ', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Đậu-Đỏ.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (6, N'Sương Sáo', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Sương-sáo.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (7, N'Thạch Trái Cây', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Thạch-trái-cây.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (8, N'Thạch Dừa', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/Thạch-Dừa.png')
+INSERT INTO [dbo].[toppings] ([MaTP], [TenTP], [DonGia], [Hinh]) VALUES (9, N'Pudding', CAST(11000.00 AS Decimal(18, 2)), N'~/Images/Topping/布丁-pudding.png')
+SET IDENTITY_INSERT [dbo].[toppings] OFF
